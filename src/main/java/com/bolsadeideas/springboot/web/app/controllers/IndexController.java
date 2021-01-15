7fcd4039e84b7863046b13f5601372ba25229c85
JonathanSanchezM <jonysanchez382@gmail.com>
2021-01-15 00:05:49 -0600

    creando controlador-vista

---
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -0,0 +1,22 @@
+package com.bolsadeideas.springboot.web.app.controllers;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/app")
+public class IndexController {
+	
+	@GetMapping({"/index", "/", "", "/home"})
+	public String index(Model model) {
+		model.addAttribute("titulo", "Hola spring framework con Model");
+		return "index";
+	}
+
+}
